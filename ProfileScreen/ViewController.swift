4bcc4c403fd70213a57dd866b6cbfba75dab322a
Shawn Li <lizijian930618@gmail.com>
2020-04-23 12:27:33 -0600

    Initial Commit

---
+++ ProfileScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProfileScreen
+//
+//  Created by Shawn Li on 4/23/20.
+//  Copyright © 2020 ShawnLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
